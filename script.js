822216133e9c01adff64b59ef837bc2aac8124dc
Priyanka Malyam <priyankamalyam@gmail.com>
2026-09-22 20:36:44 +0100

    Fix menu interaction

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-console.log("Welcome to Sheffield Café!");
-
-const contactButton = document.getElementById("contactBtn");
-
-contactButton.addEventListener("click", function () {
-    alert("Thank you for contacting Sheffield Café! We will get back to you soon.");
-});
-
-
 const menuButton = document.getElementById("menuBtn");
 const extraMenu = document.getElementById("extraMenu");
 
